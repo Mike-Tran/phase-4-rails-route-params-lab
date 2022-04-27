5eb046da5dd0c69cb1a929234a7e54499ff6b6e0
Michael Tran <mltrunner@gmail.com>
2022-04-26 17:35:31 -0700

    Completed lab with bonus

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students =  if params[:name]
+                  [Student.find_by(last_name: params[:name].capitalize)]
+                else
+                  Student.all
+                end
     render json: students
+  end
+
+  def show
+    render json: Student.find(params[:id])
   end
 
 end
